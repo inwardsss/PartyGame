1c9066bb90699a340e716e38db7d70492683080b
Kirill <141374935+inwardsss@users.noreply.github.com>
2023-10-12 22:28:57 +0300

    Update PartyGame.java

--- src/main/java/my/cufee/partygame/PartyGame.java
+++ src/main/java/my/cufee/partygame/PartyGame.java
@@ -6,7 +6,7 @@ public final class PartyGame extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Plugin startup logic hekki wirkd
 
     }
 
